c4b8070ea419ca428c547cc6305f958d446cd6aa
Mayur Hapani <hapanimayuur@gmail.com>
2024-02-27 12:36:25 +0530

    project 2 form created

--- backend/project2/frontend/src/comonent/MyBlog.jsx
+++ backend/project2/frontend/src/comonent/MyBlog.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function MyBlog() {
-  return <div>MyBlog</div>;
+  return (
+    <div className="container">
+      <div className="col-4">
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <textarea rows="10" class="form-control" id="floatingContant" placeholder="Enter Content" />
+          <label for="floatingContant">Enter Contant</label>
+        </div>
+      </div>
+    </div>
+  );
 }
--- backend/project2/frontend/src/comonent/UserCard.jsx
+++ backend/project2/frontend/src/comonent/UserCard.jsx
@@ -3,13 +3,15 @@ import Card from "react-bootstrap/Card";
 
 function UserCard() {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img className="" variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
+    <Card className="col-3">
+      <div className="p-4">
+        <Card.Img className="" style={{ width: "100%", aspectRatio: "1/1" }} variant="top" src="../../../public/vite.svg" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </div>
     </Card>
   );
 }
